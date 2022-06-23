2f4b8e54433a29f0c3f7a2aa132ffb15ba6780a6
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-06-23 15:10:30 +0200

    inital

---
+++ background.js
@@ -0,0 +1,16 @@
+/* global browser */
+
+let timerId = null;
+
+function handleCreated(tab) {
+    if(timerId !== null){
+        clearTimeout(timerId);
+    }
+    timerId = setTimeout(async function() {
+        browser.tabs.update(tab.id, { active: true });
+    }, 500);
+}
+
+browser.tabs.onCreated.addListener(handleCreated);
+
+
