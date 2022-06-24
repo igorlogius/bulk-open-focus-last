abfd63a62ff86123fde9831323e4c35b8760165b
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-06-24 17:04:57 +0200

    feat: add time bases differenciation between bulk opened tabs

--- background.js
+++ background.js
@@ -2,18 +2,33 @@
 
 let timerId = null;
 
-function handleCreated(tab) {
+// todo ignore singular tab creation
+// time between the calls between 2 created calls should be smaller then human action (aka. < 25ms)
 
-    if(!tab.openerTabId && !tab.pinned && !tab.active){
+let last_call = null;
+
+function onCreated(tab) {
+
+    const now = tab.lastAccessed; //Date.now();
+    //
+    console.log(now,last_call, now-last_call);
+    if(!tab.openerTabId && !tab.pinned && !tab.active && // this is obvious
+        last_call !== null && (now - last_call) < 25  // we assume opening many tabs simultaniously has a very short call time
+    ) {
+        // queue this tab for focusing
+
         if(timerId !== null){
             clearTimeout(timerId);
         }
         timerId = setTimeout(async function() {
             browser.tabs.update(tab.id, { active: true });
         }, 500);
+    //}
     }
+    // ignore tab for focusing
+    last_call = now;
 }
 
-browser.tabs.onCreated.addListener(handleCreated);
+browser.tabs.onCreated.addListener(onCreated);
 
 
