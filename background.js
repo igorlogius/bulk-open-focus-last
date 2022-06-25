0087bb1de45ac96760ccbb434ae08eceb3d5f6d1
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-06-25 17:11:39 +0200

    fix: missing variable unset chore: refactor & cleanup

--- background.js
+++ background.js
@@ -1,29 +1,25 @@
 /* global browser */
 
 let timerId = null;
-
-// todo ignore singular tab creation
-// time between the calls between 2 created calls should be smaller then human action (aka. < 25ms)
-
-let last_call = null;
+let last_call = Date.now();   // load time of the addon is fine i hope
 
 function onCreated(tab) {
 
-    const now = tab.lastAccessed; //Date.now();
+    const now = Date.now();
     //
-    console.log(now,last_call, now-last_call);
-    if(!tab.openerTabId && !tab.pinned && !tab.active && // this is obvious
-        last_call !== null && (now - last_call) < 25  // we assume opening many tabs simultaniously has a very short call time
+    if( !tab.openerTabId &&      //
+        !tab.pinned &&           //
+        !tab.active &&           //
+        (now - last_call) < 25   //
     ) {
         // queue this tab for focusing
-
         if(timerId !== null){
             clearTimeout(timerId);
         }
         timerId = setTimeout(async function() {
             browser.tabs.update(tab.id, { active: true });
+            timerId = null;
         }, 500);
-    //}
     }
     // ignore tab for focusing
     last_call = now;
